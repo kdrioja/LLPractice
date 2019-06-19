4948e43923b3763dc9320fc9461a48b3558fc9b1
Kenia Rioja <kdrioja@gmail.com>
2019-06-18 17:56:12 -0700

    Finished addTwoNumbers

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,36 @@ public class Main {
         c.next = null;
 
         System.out.println(listToInt(a));
+
+        ListNode d = new ListNode(5);
+        ListNode e = new ListNode(6);
+        ListNode f = new ListNode(4);
+
+        d.next = e;
+        e.next = f;
+        f.next = null;
+
+        System.out.println(listToInt(d));
+        System.out.println(listToInt(addTwoNumbers(a, d)));
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = listToInt(l1) + listToInt(l2);
+        String sumStr = Integer.toString(sum);
+
+        //at minimum sumStr has a length of 1
+        ListNode head = new ListNode(Character.getNumericValue(sumStr.charAt(sumStr.length() - 1)));
+
+        //if there are more digits
+        ListNode currentNode = head;
+
+        for (int i = sumStr.length() - 2; i >= 0; i--) {
+            ListNode newNode = new ListNode(Character.getNumericValue(sumStr.charAt(i)));
+            currentNode.next = newNode;
+            currentNode = newNode;
+        }
+
+        return head;
     }
 
     public static int listToInt(ListNode list) {
