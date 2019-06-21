924957d50ddc7ba8a9c17f5f0076570fb5546f21
Kenia Rioja <kdrioja@gmail.com>
2019-06-20 18:52:44 -0700

    Working on merging two Linked Lists

--- src/Main.java
+++ src/Main.java
@@ -28,55 +28,9 @@ public class Main {
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        //if both lists are empty
-        if (l1 == null && l2 == null) {
-            return null;
-        }
-        //if l1 si empty
-        else if (l1 == null) {
-            return l2;
-        }
-        //if l2 is empty
-        else if (l2 == null) {
-            return l1;
-        }
-        //
-        else {
-            //decide which one is the head
-            ListNode head, pointer1, pointer2;
-
-            if (l1.val <= l2.val) {
-                head = l1;
-                pointer1 = l1.next;
-                pointer2 = l2;
-            }
-            else {
-                head = l2;
-                pointer1 = l1;
-                pointer2 = l2.next;
-            }
-
-            boolean flag1 = pointer1 == null ? true : false;
-            boolean flag2 = pointer2 == null ? true : false;
-
-            while (!flag1 && !flag2) {
-                ListNode next1 = pointer1.next;
-                ListNode next2 = pointer2.next;
-
-                if (pointer1.val <= pointer2.val) {
-                    head.next = pointer1;
-                    head = pointer1;
-                }
-                else {
-                    head.next = pointer2;
-                    head = pointer2;
-                    
-                }
-            }
-
-            return l1;
-        }
+        return null;
     }
+
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int sum = listToInt(l1) + listToInt(l2);
