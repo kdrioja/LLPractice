fed80beed4dc888d8d139d015ae63ef05f600409
Kenia Rioja <kdrioja@gmail.com>
2019-06-26 11:17:49 -0700

    Wrote a print function for linked lists

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,22 @@ public class Main {
         // l1
         ListNode a = new ListNode(1);
         ListNode b = new ListNode(2);
-        ListNode c = new ListNode(4);
-
+        ListNode c = new ListNode(3);
+        // l2
+        ListNode d = new ListNode(3);
+        ListNode e = new ListNode(4);
+        ListNode f = new ListNode(4);
+        ListNode g = new ListNode(5);
         a.next = b;
         b.next = c;
-        c.next = null;
-
-        // l2
-        ListNode d = new ListNode(1);
-        ListNode e = new ListNode(3);
-        ListNode f = new ListNode(4);
-
+        c.next = d;
         d.next = e;
         e.next = f;
-        f.next = null;
+        f.next = g;
+        //g.next = null;
+
+        printList(a);
+
 
         /**
          * Testing addTwoNumbers
@@ -25,6 +27,84 @@ public class Main {
         System.out.println(listToInt(d));
         System.out.println(listToInt(addTwoNumbers(a, d)));
          */
+    }
+
+    public static void printList(ListNode head) {
+        if (head == null) {
+            System.out.println("null");
+        }
+        else {
+            while (head != null) {
+                System.out.print(head.val + "->");
+                head = head.next;
+            }
+            System.out.print("/");
+        }
+    }
+
+    /**
+     Week 4: Remove Duplicates (Challenge Problem)
+     Given a linked list, delete all nodes that have duplicate
+     numbers, leaving only distinct numbers from the original
+     list. Can you do it without taking up extra memory?
+
+     Will the numbers all be positive?
+     Will they be sorted?
+     Will they all be ints?
+     Should a null list return null?
+
+     1->1->5->5->6->6 ==> return null
+     3->6->9->9->12 ==> return 3->6->12
+
+     could use a HashSet to add the ints and check if we have
+     encountered it before
+
+     def use a dummy head to connect the unique nodes
+
+     1. if list is null, return null
+     2. create a dummy node and a tail pointer to the end of dummy
+     3. keep an int variable named lastInt which keeps the last value seen
+     4. if the next node is not equal to the value then update lastInt
+        else remove the previous node [KEEP A REFERENCE TO THE LAST NODE],
+        and current node
+
+
+     //WAIT YOU SOLVED REVERSE THE LIST NOT REMOVE DUPLICATES
+     3. Iterate through the notes
+        store prev node, current node, and next node
+        - prev is initially null
+        - current node is the head
+        - get the value of head.next and store in next
+        - set current.next to prev
+        - set prev to current
+        - set current to next node
+        - continue;
+     4. return dummyHead.next
+     */
+
+    public static ListNode removeDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode tail = dummyHead;
+        ListNode current = head;
+        int lastIntSeen = Integer.MIN_VALUE; //just a starting value
+
+        while (current != null) {
+            if (current.val == current.next.val || current.val == lastIntSeen) {
+                lastIntSeen = current.val;
+                current = current.next;
+            }
+            else {
+                lastIntSeen = current.val;
+                tail.next = current;
+                tail = current;
+            }
+        }
+
+        return dummyHead.next;
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
